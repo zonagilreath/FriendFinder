09efde8e315da1b89e2182563d50a8fe7b956fb3
Zona <bryson.gilreath@gmail.com>
2019-03-24 20:36:59 -0500

    api views working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,36 @@
 const path = require('path');
 const friends = require('../data/friends.js');
 
+function checkCompatibilty(scores1, scores2){
+    let sumDif = 0;
+    for (let i=0; i < scores1.length; i++){
+        sumDif += Math.pow(scores1[i] - scores2[i], 2);
+    }
+    return sumDif;
+}
+
+function findFriend(newUser, existingUsers){
+    let currentBestFriend = null;
+    let currentBestFriendsScore = 50;
+    existingUsers.forEach(existingUser => {
+        let friendScore = checkCompatibilty(newUser.scores, existingUser.scores);
+        if (friendScore < currentBestFriendsScore){
+            currentBestFriend = existingUser;
+            currentBestFriendsScore = friendScore;
+        }
+    });
+    return currentBestFriend;
+}
+
 module.exports = function(app){
     app.get('/api/friends', function (req, res){
         return res.json(friends);
     });
 
     app.post('/api/friends', function (req, res){
-        const newData = req.body;
-        console.log(newData);
+        const newUser = req.body;
+        let bestFriend = findFriend(newUser, friends);
+        console.log(bestFriend);
+        return res.send(bestFriend);
     });
 }
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.port || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded( { extended:true } ));
+
 require('./app/routing/htmlRoutes')(app);
 require('./app/routing/apiRoutes')(app);
 
